36bdfa30eed64ef4a408ce9f089057d2605ee8af
Eoxys1 <158499025+Eoxys1@users.noreply.github.com>
2025-03-28 09:43:34 +0100

    Update popup.js

--- APL-Malta/popup.js
+++ APL-Malta/popup.js
@@ -1,17 +1,1 @@
-function login(){
-document.getElementById("login-btn").addEventListener("click", () => {
-    console.log("login sucess");
-    
-    email = getElementById("email");
-    password = document.getElementById("password");
 
-    console.log(email, password);
-    
-    if(email == "theofranck0@gmail.com" && password == "12205667TF!"){
-
-    }else{
-        alert("login failed")
-    };
-
-}); 
-}
